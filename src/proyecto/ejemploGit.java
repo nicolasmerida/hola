1eff0fe19b6be12b4bd88adf3d3fca926763fe19
Nicolas <nicolasmerida15@gmail.com>
2023-10-01 21:49:09 -0300

    agregadas variables

--- src/proyecto/ejemploGit.java
+++ src/proyecto/ejemploGit.java
@@ -3,8 +3,8 @@ package proyecto;
 public class ejemploGit {
 	public static void main (String[] args) {
 		
-		
-		 
+		String nombre= "hola todos como estan ";
+		System.out.println("hola hola hola ") ;
 		
 	}
 
